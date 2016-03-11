25e04d57497db67e90ea714a9775b9f75ac27100
ldechamps <ldechamps@hotmail.com>
2016-03-11 14:35:21 +0100

    v2

--- app.js
+++ app.js
@@ -1,8 +1,64 @@
 var koa = require('koa');
 var app = module.exports = koa();
+var route = require('koa-route');
+var auth = require('koa-basic-auth');
 
-app.use(function *(){
+// pour debug
+var logger = require('koa-logger');
+
+// middleware
+app.use(logger());
+
+
+// middleware route
+app.use(auth({ name: 'loic', pass: 'loic' }));
+
+app.use(route.get('/', helloWorld));
+app.use(pageNotFound);
+app.use(authentification);
+
+function* authentification(next){
+  try {
+    yield* next;
+  } catch (err) {
+    if (401 == err.status) {
+      this.status = 401;
+      this.body = 'Non autorisé';
+    } else {
+      throw err;
+    }
+  }
+}
+
+function* helloWorld(){
   this.body = 'Hello World';
-});
+}
 
-if (!module.parent) app.listen(3000);
+function* pageNotFound(next){
+  yield* next;
+
+  if (404 != this.status) return;
+
+  this.status = 404;
+
+  switch (this.accepts('html', 'json')) {
+    case 'html':
+      this.type = 'html';
+      this.body = '<p>Page Not Found</p>';
+      break;
+    case 'json':
+      this.body = {
+        message: 'Page Not Found'
+      };
+      break;
+    default:
+      this.type = 'text';
+      this.body = 'Page Not Found';
+  }
+}
+
+if (!module.parent) {
+    var port = 3000;
+    console.log("ecoute sur le port : "+port)
+    app.listen(port);
+}
--- test.js
+++ test.js
@@ -1,11 +1,53 @@
 var app = require('./app');
 var request = require('supertest').agent(app.listen());
+var assert = require('assert');
+
 
 describe('Hello World', function(){
   it('devrait renvoyer "Hello World"', function(done){
     request
     .get('/')
+    .auth('tj', 'tobi')
     .expect(200)
     .expect('Hello World', done);
   });
 });
+
+describe('404', function(){
+  describe('Quand GET /toto', function(){
+    it('devrait retourner une erreur 404', function(done){
+      request
+      .get('/toto')
+      .auth('tj', 'tobi')
+      .expect(404)
+      .expect(/Page Not Found/, done);
+    })
+  })
+})
+
+describe('autorisations', function(){
+    describe('sans autorisation', function(){
+        it('devrait renvoyer 401', function(done){
+            request
+            .get('/')
+            .expect(401, done);
+        })
+    })
+    
+    describe('sans autorisation sur page inconnu', function(){
+        it('devrait renvoyer 401 au lieu de 404', function(done){
+            request
+            .get('/toto')
+            .expect(401, done);
+        })
+    })
+
+  describe('avec autorisations érronées', function(){
+    it('devrait renvoyer 401', function(done){
+      request
+        .get('/')
+        .auth('user', 'invalid password')
+        .expect(401, done);
+    })
+  })
+})
